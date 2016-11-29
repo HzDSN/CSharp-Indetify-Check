874385ed6f8c017b3b662a02e6dfea650f8bb714
dongs <dongs@DESKTOP-8R6AIJ0>
2016-11-29 21:48:36 +0800

    Add some comments.

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -27,27 +27,21 @@ namespace ConsoleApplication2
                 Console.WriteLine("Please enter Position.");
                 position = Console.ReadLine();
 
-                Citizen leader = new Leader(idNumber, fullName, dateOfBirth, faction, position);
+                Leader leader = new Leader(idNumber, fullName, dateOfBirth, faction, position);
+                leader.showCitizenInfo(); //考点：对象调用方法
+                leader.showLeaderInfo();
             }
             else
             {
-                Citizen citizen = new Citizen(idNumber, fullName, dateOfBirth);
-            }
-
-            Console.WriteLine("ID Number:{0}", idNumber);
-            Console.WriteLine("Full Name:{0}", fullName);
-            Console.WriteLine("Date of Birth:{0}", dateOfBirth);
-            if (indetify == 1)
-            {
-                Console.WriteLine("Faction:{0}", faction);
-                Console.WriteLine("Position:{0}", position);
+                Citizen citizen = new Citizen(idNumber, fullName, dateOfBirth); //考点：通过对象调用类
+                citizen.showCitizenInfo();
             }
         }
     }
 
     class Citizen
     {
-        protected string idNumber;
+        protected string idNumber; //考点：定义类的字段
         protected string fullName;
         protected DateTime dateOfBirth;
 
@@ -56,14 +50,14 @@ namespace ConsoleApplication2
 
         }
 
-        public Citizen(string id, string name, string birthday)
+        public Citizen(string id, string name, string birthday) //考点：类的构造
         {
             idNumber = id;
             fullName = name;
             dateOfBirth = Convert.ToDateTime(birthday);
         }
 
-        public void showCitizenInfo()
+        public void showCitizenInfo() //考点：定义方法
         {
             Console.WriteLine("ID:Number: " + idNumber);
             Console.WriteLine("Full Name: " + fullName);
@@ -71,12 +65,12 @@ namespace ConsoleApplication2
         }
     }
 
-    sealed class Leader : Citizen
+    sealed class Leader : Citizen //考点：类的继承
     {
         protected string faction;
         protected string position;
 
-        public Leader(string id, string name, string birthday, string party, string work)
+        public Leader(string id, string name, string birthday, string party, string work) //考点：类的构造
         {
             idNumber = id;
             fullName = name;
@@ -85,7 +79,7 @@ namespace ConsoleApplication2
             position = work;
         }
 
-        public void showLeaderInfo()
+        public void showLeaderInfo() //考点：定义方法
         {
             Console.WriteLine("Faction: " + faction);
             Console.WriteLine("Position:: " + position);
